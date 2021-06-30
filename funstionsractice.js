7a04746440a608374889e56de65d8f51fce147cb
Mia Sanchez <mia@Claudias-MacBook-Air.local>
2021-06-30 11:32:25 -0400

    second commit cleaned spaces

--- funstionsractice.js
+++ funstionsractice.js
@@ -1,9 +1,21 @@
-// write a function to calculate tax 
 
+
+
+
+
+// write a function to calculate tax 
+//here is just ading some comments
  
 
-function calculateTax (){
+
+
+function calculateTax (amount){
     const taxRate = .07
+    
+    const tesla = 45000
+
+    return (tesla * taxRate). toFixed (2)  
 }
 
-console.log (taxRate)
+console.log (claculateTax () )
+
